1808f6665466e569b35e3056cbe15877f6529a11
Salchiduende <miguelangelgomez02za@gmail.com>
2025-07-14 16:07:04 -0500

    mi cambio jiji

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import Saludo from './Saludo.Js';
+import Contador from './Contador';
+<Contador />
+ 
+
+
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hola React</h1>
+      <Saludo />
+    </div>
+  );
+}
+
+
+
+
+
+
+export default App;
+
+
